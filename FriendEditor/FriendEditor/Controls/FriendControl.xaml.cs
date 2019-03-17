36c0fc6d6c099c69ed6204d21fdaf197b06da2a8
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-03-17 21:45:33 +0900

    FriendControl partial 클래스 상속클래스 변경 ( : Window => : UserControl )

--- FriendEditor/FriendEditor/Controls/FriendControl.xaml.cs
+++ FriendEditor/FriendEditor/Controls/FriendControl.xaml.cs
@@ -17,7 +17,7 @@ namespace FriendEditor.Controls
     /// <summary>
     /// FriendControl.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class FriendControl : Window
+    public partial class FriendControl : UserControl
     {
         public FriendControl()
         {
