90f1480fc78804dd466ee69ff937e618ea13120f
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-03-24 18:35:38 +0900

    Services 폴더내의 IDataProvider 인터페이스 작성 완료

--- FriendEditor/FriendEditor/Services/IDataProvider.cs
+++ FriendEditor/FriendEditor/Services/IDataProvider.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using FriendEditor.Models;
 
 namespace FriendEditor.Services
 {
-    interface IDataProvider
+    public interface IDataProvider
     {
+        bool Delete(IFriend friend);
+
+        List<IFriend> GetAllFriends();
+
+        IFriend GetFriendById(string id);
+
+        bool Insert(IFriend friend);
+
+        bool Update(IFriend friend);
     }
 }
