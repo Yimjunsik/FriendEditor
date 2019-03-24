a364e2a968d825c877a1804eea90d463465e2b29
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-03-24 16:41:14 +0900

    MainViewModel 클래스에 _allFriends, _selectedFriend 변수 추가

--- FriendEditor/FriendEditor/ViewModels/MainViewModel.cs
+++ FriendEditor/FriendEditor/ViewModels/MainViewModel.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using FriendEditor.Models;
+
 
 namespace FriendEditor.ViewModels
 {
-    class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        #region Variables
+
+        private ObservableCollection<Friend> _allFriends;
+        private Friend _selectedFriend;
+
+        #endregion
     }
 }
