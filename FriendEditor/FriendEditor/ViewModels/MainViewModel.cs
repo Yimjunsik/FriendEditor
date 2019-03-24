50a9b3f023f27495d0096dd250108cd57ced5c37
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-03-24 19:20:34 +0900

    MainViewModel 클래스 Properties 영역 추가

--- FriendEditor/FriendEditor/ViewModels/MainViewModel.cs
+++ FriendEditor/FriendEditor/ViewModels/MainViewModel.cs
@@ -7,6 +7,7 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System.Collections.ObjectModel;
 using FriendEditor.Models;
+using FriendEditor.Services;
 
 
 namespace FriendEditor.ViewModels
@@ -19,5 +20,51 @@ namespace FriendEditor.ViewModels
         private Friend _selectedFriend;
 
         #endregion
+
+        #region Constructors
+
+        public MainViewModel(IDataProvider dataProvider, IEditWindowController editWindowController, IDialogService dialogService)
+        {
+            DataProvider = dataProvider;
+
+        }
+        #endregion
+
+        #region Properties
+
+        public RelayCommand AddFriendCommand { get; set; }
+
+        /// <summary>
+        /// Get or set AllFriends value
+        /// </summary>
+        public ObservableCollection<Friend> AllFriends
+        {
+            get { return _allFriends; }
+            set { Set(ref _allFriends, value); }
+        }
+
+        public IDataProvider DataProvider { get; }
+        public RelayCommand<Friend> DeleteFriendCommand { get; set; }
+        public IDialogService DialogService { get; }
+        public RelayCommand<Friend> EditFriendCommand { get; set; }
+        public IEditWindowController EditWindowController { get; }
+
+        /// <summary>
+        /// Get or set SelectedFriend value
+        /// </summary>
+        public Friend SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set
+            {
+                Set(ref _selectedFriend, value);
+
+                // If SelectedFriend property changed, check if Edit & Delete commands can execute
+                DeleteFriendCommand.RaiseCanExecuteChanged();
+                EditFriendCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        #endregion Properties
     }
 }
