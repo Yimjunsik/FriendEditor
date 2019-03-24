97797f10d610977b3778fae14ed42821c3fcfd25
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-03-24 21:11:32 +0900

    Services 폴더 내에 EditWindowController 클래스 작성 완료

--- FriendEditor/FriendEditor/Services/EditWindowController.cs
+++ FriendEditor/FriendEditor/Services/EditWindowController.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FriendEditor.EventArgs;
+using FriendEditor.Views;
+using GalaSoft.MvvmLight.Ioc;
 
 namespace FriendEditor.Services
 {
-    class EditWindowController
+    public class EditWindowController : IEditWindowController
     {
+        /// <summary>
+        /// Show EditWindow
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public bool? ShowDialog(OpenEditWindowArgs args)
+        {
+            // If the container contains then target Type, then remove it firstly, or else an Exception will be thrown here
+            if (SimpleIoc.Default.ContainsCreated<OpenEditWindowArgs>())
+            {
+                SimpleIoc.Default.Unregister<OpenEditWindowArgs>();
+            }
+
+            SimpleIoc.Default.Register(() => args);
+
+            EditWindow editWindow = new EditWindow();
+            return editWindow.ShowDialog();
+        }
     }
 }
