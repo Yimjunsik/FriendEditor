78cd7becc0eb0db5b8678b6fa5958e8eb4c29f68
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-03-24 20:43:53 +0900

    ViewModels 폴더 내의 EditViewModel 클래스 생성자 작성 완료

--- FriendEditor/FriendEditor/ViewModels/EditViewModel.cs
+++ FriendEditor/FriendEditor/ViewModels/EditViewModel.cs
@@ -10,6 +10,38 @@ namespace FriendEditor.ViewModels
 {
     public class EditViewModel : ViewModelBase
     {
+        #region Constructors
+
+        public EditViewModel(OpenEditWindowArgs args, IDataProvider dataProvider, IDialogService dialogService)
+        {
+            Args = args;
+            DataProvider = dataProvider;
+            DialogService = dialogService;
+
+            switch (args.Type)
+            {
+                case ActionType.Add:
+                    CurrentFriend = new Friend { Id = Guid.NewGuid().ToString(), BirthDate = new DateTime(1990, 1, 1) };
+                    break;
+
+                case ActionType.Edit:
+                    // Clone a new object
+                    CurrentFriend = new Friend
+                    {
+                        Id = args.Friend.Id,
+                        Name = args.Friend.Name,
+                        BirthDate = args.Friend.BirthDate,
+                        Email = args.Friend.Email,
+                        IsDeveloper = args.Friend.IsDeveloper
+                    };
+                    break;
+            }
+
+            SaveDataCommand = new RelayCommand(SaveData);
+        }
+
+        #endregion Constructors
+
         #region Properties
 
         public Friend CurrentFriend { get; set; }
