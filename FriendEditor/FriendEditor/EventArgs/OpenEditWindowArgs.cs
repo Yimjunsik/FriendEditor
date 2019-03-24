ccb683e7497ce7fa7df331365fb6cd52af635ef4
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-03-24 18:53:53 +0900

    EventArgs 폴더 내에 OpenEditWindowArgs 클래스 작성 완료

--- FriendEditor/FriendEditor/EventArgs/OpenEditWindowArgs.cs
+++ FriendEditor/FriendEditor/EventArgs/OpenEditWindowArgs.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FriendEditor.Models;
 
 namespace FriendEditor.EventArgs
 {
-    class OpenEditWindowArgs
+    /// <summary>
+    /// Action Type
+    /// </summary>
+    public enum ActionType
     {
+        /// <summary>
+        /// Add a friend
+        /// </summary>
+        Add,
+
+        /// <summary>
+        /// Edit a friend
+        /// </summary>
+        Edit
+    }
+
+    /// <summary>
+    /// Open EditWindow arguments
+    /// </summary>
+    public class OpenEditWindowArgs
+    {
+        /// <summary>
+        /// If <see cref="ActionType"/> is Edit, then the value for this property need to be provided
+        /// </summary>
+        public Friend Friend { get; set; }
+
+        public ActionType Type { get; set; }
     }
 }
