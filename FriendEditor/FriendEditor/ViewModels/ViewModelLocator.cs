ad8df86c74a01f69d30e15554cf5af948274dda6
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-03-24 20:46:23 +0900

    ViewModels 폴더 내의 ViewModelLocator 클래스 작성 완료

--- FriendEditor/FriendEditor/ViewModels/ViewModelLocator.cs
+++ FriendEditor/FriendEditor/ViewModels/ViewModelLocator.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Ioc;
 
 namespace FriendEditor.ViewModels
 {
-    class ViewModelLocator
+    public class ViewModelLocator
     {
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<EditViewModel>();
+        }
+
+        public EditViewModel EditViewModel => SimpleIoc.Default.GetInstance<EditViewModel>(Guid.NewGuid().ToString());
+        public MainViewModel MainViewModel => SimpleIoc.Default.GetInstance<MainViewModel>(Guid.NewGuid().ToString());
     }
 }
