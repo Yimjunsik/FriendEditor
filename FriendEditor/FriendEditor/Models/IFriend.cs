f61af2a91ab7280fadac9f9d02efc55ae7f7f000
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-03-24 15:54:12 +0900

    Models 폴더내 IFriend 인터페이스 작성 완료

--- FriendEditor/FriendEditor/Models/IFriend.cs
+++ FriendEditor/FriendEditor/Models/IFriend.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace FriendEditor.Models
 {
-    interface IFriend
+    public interface IFriend
     {
+        DateTime BirthDate { get; set; }
+        string Email { get; set; }
+        string Id { get; set; }
+        bool IsDeveloper { get; set; }
+
+        string Name { get; set; }
     }
 }
