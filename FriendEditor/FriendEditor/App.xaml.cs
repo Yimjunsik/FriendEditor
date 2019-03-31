578a50f102334d4506b143a0210014809fddf00f
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-03-31 16:18:33 +0900

    App.xaml 클래스 내, Application_Startup() 메서드 작성 완료

--- FriendEditor/FriendEditor/App.xaml.cs
+++ FriendEditor/FriendEditor/App.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FriendEditor.Services;
+using FriendEditor.Views;
+using GalaSoft.MvvmLight.Ioc;
 using System.Windows;
 
 namespace FriendEditor
@@ -13,5 +10,21 @@ namespace FriendEditor
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            SimpleIoc.Default.Register<IDataProvider, SqliteDataProvider>();
+            SimpleIoc.Default.Register<IEditWindowController, EditWindowController>();
+            SimpleIoc.Default.Register<IDialogService, DialogService>();
+
+            App.Current.DispatcherUnhandledException += (s, args) =>
+            {
+                SimpleIoc.Default.GetInstance<IDialogService>().Exception(args.Exception);
+                args.Handled = true;
+            };
+
+            MainWindow mainWindow = new MainWindow();
+            App.Current.MainWindow = mainWindow;
+            mainWindow.Show();
+        }
     }
 }
