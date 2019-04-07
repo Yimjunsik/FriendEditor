019c65257f53115fe4b0704389ad4b71c4bde590
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-04-07 16:29:51 +0900

    BooleanToVisibilityConverter 클래스 접근제한자 public 추가

--- FriendEditor/FriendEditor/Converters/BooleanToVisibilityConverter.cs
+++ FriendEditor/FriendEditor/Converters/BooleanToVisibilityConverter.cs
@@ -9,7 +9,7 @@ using System.Windows;
 
 namespace FriendEditor.Converters
 {
-    class BooleanToVisibilityConverter : IValueConverter
+    public class BooleanToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
