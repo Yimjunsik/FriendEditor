d17304ff954a83c859154801caa893ba942268f1
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-04-07 15:36:49 +0900

    Views/EditWindow.xaml.cs 클래스내 CloseWindow(), EditWindow_Unloaded() 메소드 작성 완료 및 EditWindow() 생성자에 정보 추가

--- FriendEditor/FriendEditor/Views/EditWindow.xaml.cs
+++ FriendEditor/FriendEditor/Views/EditWindow.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FriendEditor.EventArgs;
+using GalaSoft.MvvmLight.Messaging;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace FriendEditor.Views
 {
@@ -22,6 +13,19 @@ namespace FriendEditor.Views
         public EditWindow()
         {
             InitializeComponent();
+            Unloaded += EditWindow_Unloaded;
+            Messenger.Default.Register<CloseWindowEventArgs>(this, CloseWindow);
+        }
+
+        private void CloseWindow(CloseWindowEventArgs obj)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void EditWindow_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Messenger.Default.Unregister<CloseWindowEventArgs>(this);
         }
     }
 }
